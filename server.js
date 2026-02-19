dc12907d8995eaa3b912c05179168a10e7cc73e4
6rand0n <brandonjavier03@gmail.com>
2026-02-19 13:01:48 -0600

    docs: add explanatory comments (no logic changes)

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+// Importar dependencias necesarias
 const express = require('express');
 const { createYoga } = require('graphql-yoga');
 const { connectDB } = require('./config/db');
 const registroRoutes = require('./routes/registro.routes');
 const { schema } = require('./graphql/schema');
 
+// Crear la aplicación Express
 const app = express();
-
+// Middleware para parsear JSON
 app.use(express.json());
 
 async function startServer() {
+  // Conectar a la base de datos
   await connectDB();
 
   // REST
@@ -20,8 +23,8 @@ async function startServer() {
     graphqlEndpoint: '/graphql'
   });
 
+  // Integrar GraphQL Yoga con Express e iniciar el servidor
   app.use(yoga);
-
   app.listen(4000, () => {
     console.log('Servidor corriendo en puerto 4000');
   });
