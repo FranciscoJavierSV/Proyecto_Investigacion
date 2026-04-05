68cca4ab5324039abc5e1065b30666c2a524c4c2
6rand0n <brandonjavier03@gmail.com>
2026-04-05 13:27:42 -0600

    Grafana integrada a Graph

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 require('dotenv').config();
+
 const express = require('express');
 const { createYoga } = require('graphql-yoga');
+const { performance } = require('perf_hooks');
+const client = require('prom-client');
+
 const { connectDB, getDB } = require('./config/db');
 const { schema } = require('./graphql/schema');
 
+const {
+	httpRequestDuration,
+	httpRequestsTotal,
+	payloadSize,
+	ttfbMetric,
+	errorsTotal,
+	cpuUsage,
+	memoryUsage
+} = require('./metrics');
+
+// REST ROUTES
 const allRoutes = require('./restapi/routes/all.routes');
 const clienteRoutes = require('./restapi/routes/cliente.routes');
 const productoRoutes = require('./restapi/routes/producto.routes');
@@ -18,95 +33,91 @@ app.use(express.json());
 
 async function startServer() {
 
-	connectDB().then(() => {
-		const db = getDB();
+	await connectDB();
+	const db = getDB();
 
-		const yoga = createYoga({
-			schema,
-			context: () => ({
-				db
-			})
-		});
+	// ✅ UNA sola instancia de Yoga con métricas
+	const yoga = createYoga({
+		schema,
 
-		//app.use('/api/registros', registroRoutes);
-		app.use('/graphql', yoga);
+		context: ({ request }) => ({
+			db,
+			request
+		}),
 
-		// REST ROUTES
-		app.use('/rest/', allRoutes);
-		app.use('/rest/clientes', clienteRoutes);
-		app.use('/rest/datosfacturas', datosFacturaRoutes);
-		app.use('/rest/facturas', facturaRoutes);
-		app.use('/rest/productos', productoRoutes);
-		app.use('/rest/variaciones', variacionRoutes);
+		plugins: [
+			{
+				onRequest({ request }) {
+					request.startTime = performance.now();
+					request.cpuStart = process.cpuUsage();
+				},
 
-		app.listen(PORT, () => {
-			console.log(`Servidor corriendo en puerto ${PORT}`);
-			console.log(`GraphQL: http://localhost:${PORT}/graphql`);
-			console.log(`API REST: http://localhost:${PORT}/rest`);
-		});
+				onExecute() {
+					const ttfbStart = performance.now();
+
+					return {
+						onExecuteDone({ result, args }) {
+
+							const req = args.contextValue.request;
+
+							const duration = performance.now() - req.startTime;
+
+							// 📊 RESPONSE TIME
+							httpRequestDuration.observe(duration);
+
+							// 📊 THROUGHPUT
+							httpRequestsTotal.inc();
+
+							// 📊 PAYLOAD SIZE
+							const size = Buffer.byteLength(JSON.stringify(result));
+							payloadSize.observe(size);
+
+							// 📊 TTFB
+							ttfbMetric.observe(performance.now() - ttfbStart);
+
+							// 📊 CPU
+							const cpuEnd = process.cpuUsage(req.cpuStart);
+							const cpuMs = (cpuEnd.user + cpuEnd.system) / 1000;
+							cpuUsage.set(cpuMs);
+
+							// 📊 MEMORY
+							const mem = process.memoryUsage().heapUsed;
+							memoryUsage.set(mem);
+
+							// 📊 ERRORS
+							if (result.errors) {
+								errorsTotal.inc(result.errors.length);
+							}
+						}
+					};
+				}
+			}
+		]
+	});
+
+	// GRAPHQL
+	app.use('/graphql', yoga);
+
+	// REST
+	app.use('/rest/', allRoutes);
+	app.use('/rest/clientes', clienteRoutes);
+	app.use('/rest/datosfacturas', datosFacturaRoutes);
+	app.use('/rest/facturas', facturaRoutes);
+	app.use('/rest/productos', productoRoutes);
+	app.use('/rest/variaciones', variacionRoutes);
+
+	// 📊 METRICS ENDPOINT (Prometheus)
+	app.get('/metrics', async (req, res) => {
+		res.set('Content-Type', client.register.contentType);
+		res.end(await client.register.metrics());
+	});
+
+	app.listen(PORT, () => {
+		console.log(`Servidor corriendo en puerto ${PORT}`);
+		console.log(`GraphQL: http://localhost:${PORT}/graphql`);
+		console.log(`REST: http://localhost:${PORT}/rest`);
+		console.log(`Metrics: http://localhost:${PORT}/metrics`);
 	});
 }
-
-const {
-	httpRequestDuration,
-	httpRequestsTotal,
-	payloadSize,
-	ttfbMetric,
-	errorsTotal,
-	cpuUsage,
-	memoryUsage
-} = require('./metrics');
-
-const yoga = createYoga({
-	schema,
-	plugins: [
-		{
-			onRequest({ request }) {
-				request.startTime = performance.now();
-				request.cpuStart = process.cpuUsage();
-			},
-
-			onExecute() {
-				const ttfbStart = performance.now();
-
-				return {
-					onExecuteDone({ result, args }) {
-
-						const req = args.contextValue.request;
-
-						const duration = performance.now() - req.startTime;
-
-						// RESPONSE TIME
-						httpRequestDuration.observe(duration);
-
-						// THROUGHPUT
-						httpRequestsTotal.inc();
-
-						// PAYLOAD SIZE
-						const size = Buffer.byteLength(JSON.stringify(result));
-						payloadSize.observe(size);
-
-						// TTFB
-						ttfbMetric.observe(performance.now() - ttfbStart);
-
-						// CPU
-						const cpuEnd = process.cpuUsage(req.cpuStart);
-						const cpuPercent = (cpuEnd.user + cpuEnd.system) / 1000;
-						cpuUsage.set(cpuPercent);
-
-						// MEMORY
-						const mem = process.memoryUsage().heapUsed;
-						memoryUsage.set(mem);
-
-						// ERRORS
-						if (result.errors) {
-							errorsTotal.inc(result.errors.length);
-						}
-					}
-				};
-			}
-		}
-	]
-});
 
 startServer();
